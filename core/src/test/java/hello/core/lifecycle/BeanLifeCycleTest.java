bda8f7f15144bd22304e51299f478fff0016cfca
clzlol <nyc0527@naver.com>
2024-07-15 15:01:08 +0900

    빈 생명주기 콜백 빈이 초기화~종료 되는 과정에 대해 알아봄. @PostConstruct, @PreDestroy를 사용하고, 외부 라이브러리를 초기화, 종료해야 하면 @Bean의 initMethod, destroyMethod 사용하기.

---
+++ core/src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -0,0 +1,28 @@
+package hello.core.lifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BeanLifeCycleTest {
+
+    @Test
+    public void lifeCycleTest() {
+        ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+        NetworkClient client = ac.getBean(NetworkClient.class);
+        ac.close();
+    } // 스프링 빈은 객체 생성을 한 후 의존관계 주입을 하는 라이프 사이클을 가진다. (생성자 주입은 제외)
+
+    @Configuration
+    static class LifeCycleConfig {
+
+        @Bean
+        public NetworkClient networkClient() {
+            NetworkClient networkClient = new NetworkClient();
+            networkClient.setUrl("http://hello-spring.dev");
+            return networkClient;
+        }
+    }
+}
